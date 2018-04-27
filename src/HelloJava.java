2b30e17152073a7f1dc33ae0029fdcedcfa925af
alexkaufman06 <alexkaufman06@gmail.com>
2018-04-26 20:54:50 -0700

    Add initial files

---
+++ src/HelloJava.java
@@ -0,0 +1,9 @@
+
+public class HelloJava {
+	public static void main(String[] args) throws InterruptedException {
+		for (int i=1;i<11;i++) {
+			System.out.println(i + ") Hellow Java!");
+			Thread.sleep(1000);
+		}
+	}
+}
